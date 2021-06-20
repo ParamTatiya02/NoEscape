668d93be5b0eaab31f307dc85e1ba3f4a68ce483
ParamTatiya02 <paramtatiya@gmail.com>
2021-06-20 19:20:59 +0530

    No escape website

---
+++ NoEscape/accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+
+class Applicant(models.Model):
+    name = models.CharField(max_length=100, default='add name')
+    phone = models.IntegerField(default='add number')
+    email = models.EmailField(default='abc@edf.com')
+    complaint = models.CharField(max_length=200, default='add address')
+
+    def __str__(self):
+        return self.name
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=100, default='add name')
+    phone = models.IntegerField(default='add number')
+    email = models.EmailField(default='abc@edf.com')
+    video = models.FileField(upload_to='videos/', null=True)
+
+    def __str__(self):
+        return self.name
---
+++ NoEscape/accounts/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .models import Video, Applicant
+from .forms import Video_form, Complaint, CreateUserForm
+
+
+def home(request):
+    return render(request, '../templates/NoEscape/home.html')
+
+
+def main(request):
+    return render(request, '../templates/NoEscape/main.html')
+
+
+
+def log_in(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.info(request, 'Username or Password is Incorrect')
+
+    context = {}
+    return render(request, '../templates/NoEscape/login.html', context)
+
+
+def sign_up(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+    context = {'form': form}
+    return render(request, '../templates/NoEscape/sign_up.html', context)
+
+
+def contact(request):
+    return render(request, '../templates/NoEscape/contact.html')
+
+
+def complaint(request):
+    form = Applicant()
+    if request.method == 'POST':
+        form = Complaint(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Complaint Successfully Registered')
+            return redirect('../profile')
+    context = {'form': form}
+    return render(request, '../templates/NoEscape/complaint.html', context)
+
+
+def about(request):
+    return render(request, '../templates/NoEscape/about.html')
+
+
+def evidence(request):
+    all_video = Video.objects.all()
+    if request.method == "POST":
+        form = Video_form(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Evidence Successfully Uploaded')
+            return redirect('main')
+    else:
+        form = Video_form()
+    return render(request, '../templates/NoEscape/evidence.html', {"form": form, "all": all_video})
